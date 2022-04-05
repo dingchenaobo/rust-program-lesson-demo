588f0c3645d66e4702cc25fb665e45b960b29be1
dingchen <dingchenaobo@163.com>
2022-04-05 18:48:58 +0800

    feat: rust code init

--- demo2_HTTPie/src/main.rs
+++ demo2_HTTPie/src/main.rs
@@ -1,3 +1,35 @@
+use clap::{Parser, Subcommand};
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+struct Opts {
+    #[clap(subcommand)]
+    actiion: Action,
+}
+
+#[derive(Subcommand, Debug)]
+enum Action {
+    GET(Get),
+    POST(Post),
+}
+
+/// get 请求
+#[derive(Parser, Debug)]
+struct Get {
+    /// HTTP 请求的 URL
+    url: String,
+}
+
+/// post 请求
+#[derive(Parser, Debug)]
+struct Post {
+    /// HTTP 请求的 URL
+    url: String,
+    /// HTTP 请求的 body
+    body: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts: Opts = Opts::parse();
+    println!("{:?}", opts);
 }
