57fdac069923d659a592ec7eb02d7d0cacda177a
dingchen <dingchenaobo@163.com>
2022-04-06 22:27:47 +0800

    feat: 添加异步请求处理逻辑

--- demo2_HTTPie/src/main.rs
+++ demo2_HTTPie/src/main.rs
@@ -1,6 +1,7 @@
+use anyhow::{anyhow, Result};
 use clap::{Parser, Subcommand};
-use anyhow::Result;
-use reqwest::Url;
+use reqwest::{header, Client, Response, Url};
+use std::{collections::HashMap, str::FromStr};
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -27,9 +28,11 @@ struct Get {
 #[derive(Parser, Debug)]
 struct Post {
     /// HTTP 请求的 URL
+    #[clap(parse(try_from_str = parse_url))]
     url: String,
     /// HTTP 请求的 body
-    body: Vec<String>,
+    #[clap(parse(try_from_str = parse_kv_pair))]
+    body: Vec<KVPair>,
 }
 
 fn parse_url(_url: &str) -> Result<String> {
@@ -37,7 +40,53 @@ fn parse_url(_url: &str) -> Result<String> {
     Ok(url.into())
 }
 
-fn main() {
+#[derive(Debug)]
+struct KVPair {
+    k: String,
+    v: String,
+}
+
+impl FromStr for KVPair {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut split = s.split("=");
+        let err = || anyhow!(format!("Failed to parse {}", s));
+        Ok(Self {
+            k: (split.next().ok_or_else(err)?).to_string(),
+            v: (split.next().ok_or_else(err)?).to_string(),
+        })
+    }
+}
+
+fn parse_kv_pair(_s: &str) -> Result<KVPair> {
+    Ok(_s.parse()?)
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
     let opts: Opts = Opts::parse();
     println!("{:?}", opts);
+    let client = Client::new();
+    let result = match opts.actiion {
+        Action::GET(ref args) => get(client, args).await?,
+        Action::POST(ref args) => post(client, args).await?,
+    };
+    Ok(result)
+}
+
+async fn get(client: Client, args: &Get) -> Result<()> {
+    let res = client.get(&args.url).send().await?;
+    println!("{:?}", res.text().await?);
+    Ok(())
+}
+
+async fn post(client: Client, args: &Post) -> Result<()> {
+    let mut body = HashMap::new();
+    for pair in args.body.iter() {
+        body.insert(&pair.k, &pair.v);
+    }
+    let res = client.post(&args.url).json(&body).send().await?;
+    println!("{:?}", res.text().await?);
+    Ok(())
 }
